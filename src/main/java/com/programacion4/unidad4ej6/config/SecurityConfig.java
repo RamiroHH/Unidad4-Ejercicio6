30112384a9bb60afc96405000e5d3efca9ae55f8
Juan Pablo Dealbera <80698781+juanpid2112@users.noreply.github.com>
2026-03-31 21:18:47 -0300

    fix

--- src/main/java/com/programacion4/unidad4ej6/config/SecurityConfig.java
+++ src/main/java/com/programacion4/unidad4ej6/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/productos/**", "/h2-console/**").permitAll()
+                        .requestMatchers("/insumos/**", "/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
