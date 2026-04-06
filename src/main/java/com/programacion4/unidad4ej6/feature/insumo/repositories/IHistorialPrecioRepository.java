6ea7d70339c28a3f8d94574f9906d54709d4c797
RamiroHH <ramiroh.dev@gmail.com>
2026-04-06 19:51:03 -0300

    Update IHistorialPrecioRepository.java

--- src/main/java/com/programacion4/unidad4ej6/feature/insumo/repositories/IHistorialPrecioRepository.java
+++ src/main/java/com/programacion4/unidad4ej6/feature/insumo/repositories/IHistorialPrecioRepository.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Repository;
 
 import com.programacion4.unidad4ej6.feature.insumo.models.HistorialPrecio;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IHistorialPrecioRepository extends CrudRepository<HistorialPrecio, Long> {
     Optional<HistorialPrecio> findTopByInsumoIdOrderByFechaDesc(Long insumoId);
+    List<HistorialPrecio> findTop5ByInsumoIdOrderByFechaDesc(Long insumoId);
 }
