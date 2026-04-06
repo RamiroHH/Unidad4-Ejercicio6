b96e7b5b4ec0ec62825bdf96e6b99a4f23e152c3
RamiroHH <ramiroh.dev@gmail.com>
2026-04-06 19:51:09 -0300

    Create InsumoRestoreService.java

---
+++ src/main/java/com/programacion4/unidad4ej6/feature/insumo/services/impl/domain/InsumoRestoreService.java
@@ -0,0 +1,26 @@
+package com.programacion4.unidad4ej6.feature.insumo.services.impl.domain;
+
+import com.programacion4.unidad4ej6.config.exceptions.NotFoundException;
+import com.programacion4.unidad4ej6.feature.insumo.models.Insumo;
+import com.programacion4.unidad4ej6.feature.insumo.repositories.IInsumoRepository;
+import com.programacion4.unidad4ej6.feature.insumo.services.interfaces.domain.IInsumoRestoreService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class InsumoRestoreService implements IInsumoRestoreService {
+
+    private final IInsumoRepository insumoRepository;
+
+    @Override
+    public void restore(Long id) {
+
+        Insumo insumo = insumoRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Insumo no encontrado"));
+
+        insumo.setActivo(true);
+        insumoRepository.save(insumo);
+    }
+}
